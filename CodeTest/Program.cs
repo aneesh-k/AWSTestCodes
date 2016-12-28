cd48413a067e0842bac9857bb383b98ef5ffa73f
Aneesh-k <aneeshkandagatla@gmail.com>
2016-12-28 14:16:41 +0530

    check in

--- CodeTest/Program.cs
+++ CodeTest/Program.cs
@@ -22,9 +22,9 @@ namespace CodeTest
         {
             //Console.Write(GetServiceOutput());
            
-            //string buckName = "elasticbeanstalk-us-west-2-199824416412";
-            //AmazonS3.AmazonS3 s3 = new AmazonS3.AmazonS3();
-            //s3.DeleteBucket(buckName);
+            string buckName = "elasticbeanstalk-us-west-2-199824416412";
+            AmazonS3.AmazonS3 s3 = new AmazonS3.AmazonS3();
+            s3.DeleteBucket(buckName);
 
             //Console.WriteLine("Done");
             Console.Read();
